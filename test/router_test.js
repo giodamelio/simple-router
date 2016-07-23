49009100719e876c977213119e4b7a5b8dd32e84
Gio d'Amelio <giodamelio@gmail.com>
2016-07-22 20:42:32 -0700

    Test each method type

--- test/router_test.js
+++ test/router_test.js
@@ -11,17 +11,27 @@ describe('Router', () => {
     expect(router).to.be.an.instanceof(Router);
   });
 
-  it('Creates simple route', () => {
-    const router = new Router();
+  describe('Test each method type', () => {
+    // Don't test CONNECT method because it works a bit different then the rest
+    http.METHODS.splice(http.METHODS.indexOf('CONNECT'), 1);
+    for (const method of http.METHODS) {
+      it(`Test ${method} method`, () => {
+        const router = new Router();
 
-    router.get('/', (req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello World!');
-    });
+        router[method.toLowerCase()]('/', (req, res) => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end('Hello World!');
+        });
 
-    return supertest(http.createServer(router.route()))
-      .get('/')
-      .expect(200)
-      .expect('Content-Type', 'text/plain');
+        return supertest(http.createServer(router.route()))[method.toLowerCase()]('/')
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .then((res) => {
+            if (res.req.method !== 'HEAD') {
+              expect(res.text).to.equal('Hello World!');
+            }
+          });
+      });
+    }
   });
 });
