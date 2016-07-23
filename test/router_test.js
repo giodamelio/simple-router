0a33f4c648b396fbeeda52c082242c6db0647146
Gio d'Amelio <giodamelio@gmail.com>
2016-07-22 19:42:33 -0700

    Add simple route handleing

--- test/router_test.js
+++ test/router_test.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const expect = require('chai').expect;
+const supertest = require('supertest');
 
 const Router = require('../');
 
@@ -8,5 +9,20 @@ describe('Router', () => {
   it('Creates a router', () => {
     const router = new Router();
     expect(router).to.be.an.instanceof(Router);
+  });
+
+  it('Creates simple route', (done) => {
+    const router = new Router();
+
+    router.get('/', (req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello World!');
+    });
+
+    supertest(http.createServer(router.route()))
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/plain')
+      .end(done);
   });
 });
