569d6dd7fd1082f6cd939b7ea0594be0b470393c
Gio d'Amelio <giodamelio@gmail.com>
2016-07-23 17:25:20 -0700

    Basic callback chaining

--- test/router_test.js
+++ test/router_test.js
@@ -27,6 +27,29 @@ describe('Router', () => {
       .expect('404 not found');
   });
 
+  describe('Chain multiple callbacks', () => {
+    it('Multiple callbacks', () => {
+      const router = new Router();
+
+      router.get('/', (req, res) => {
+        return new Promise((resolve) => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          setTimeout(() => {
+            resolve();
+          }, 1000);
+        });
+      }, (req, res) => {
+        res.end('Hello World!');
+      });
+
+      return supertest(http.createServer(router.route()))
+        .get('/')
+        .expect('Content-Type', 'text/plain')
+        .expect(200)
+        .expect('Hello World!');
+    });
+  });
+
   describe('Test each method type', () => {
     // Don't test CONNECT method because it works a bit different then the rest
     http.METHODS.splice(http.METHODS.indexOf('CONNECT'), 1);
