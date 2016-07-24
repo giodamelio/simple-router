7396139a79359c1a4a1c4cd850a4ec492ed7b8e4
Gio d'Amelio <giodamelio@gmail.com>
2016-07-23 17:50:03 -0700

    Handle case where handler does not return a promise

--- test/router/chainedCallbacks_test.js
+++ test/router/chainedCallbacks_test.js
@@ -6,7 +6,7 @@ const supertest = require('supertest-as-promised');
 const Router = require('../../');
 
 describe('Router - Chain multiple callbacks', () => {
-  it('Multiple callbacks', () => {
+  it('Handler returns a promise', () => {
     const router = new Router();
 
     router.get('/', (req, res) => (
@@ -17,6 +17,22 @@ describe('Router - Chain multiple callbacks', () => {
         }, 1000);
       })
     ), (req, res) => {
+      res.end('Hello World!');
+    });
+
+    return supertest(http.createServer(router.route()))
+      .get('/')
+      .expect('Content-Type', 'text/plain')
+      .expect(200)
+      .expect('Hello World!');
+  });
+
+  it.only('Handler does not return promise', () => {
+    const router = new Router();
+
+    router.get('/', (req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+    }, (req, res) => {
       res.end('Hello World!');
     });
 
